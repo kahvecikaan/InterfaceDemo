186758259d02a752fc622e962176e653e10f271e
Kaan Kahveci <kaankahveci29@outlook.com>
2023-08-21 11:28:14 +0300

    Some minor changes

--- InterfaceDemo/Entities/Player.cs
+++ InterfaceDemo/Entities/Player.cs
@@ -2,9 +2,9 @@ namespace InterfaceDemo.Entities;
 
 public class Player
 {
-    public string TcNo { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? TcNo { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public int BirthYear { get; set; }
     public List<Game> PurchasedGames { get; set; } = new List<Game>();
 }
